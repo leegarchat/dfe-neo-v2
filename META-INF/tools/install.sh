d67ddcf2ccb11c86f05bb6e30a5eb06ad596ab7e
Alexey Merzlyakov - LeeGarChat <leegarchat@gmail.com>
2024-03-03 20:40:14 +0300

    add later test fix A-only and others

--- META-INF/tools/install.sh
+++ META-INF/tools/install.sh
@@ -13,11 +13,9 @@ MAGISK_ZIP=""
 NEO_VERSION="DFE NEO 2.5.x"
 LOGNEO="$TMPN/outneo.log"
 
-if [ -n "$EXEMPLE_VERSION" ] ; then
+if [[ -n "$EXEMPLE_VERSION" ]] ; then
     NEO_VERSION="DFE-NEO $EXEMPLE_VERSION"
 fi
-
-
 
 
 if echo "$(basename "$ZIPARG3")" | $TOOLS/busybox grep -qi "extconfig"; then
@@ -36,10 +34,10 @@ fi
 
 
 
-if ! [ -f "$TMPN/unzip/META-INF/tools/languages/$languages/$languages.sh" ] ; then
+if ! [[ -f "$TMPN/unzip/META-INF/tools/languages/$languages/$languages.sh" ]] ; then
     my_print "- $languages.sh language file not found. English will be used"
     languages=en
-    ! [ -f "$TMPN/unzip/META-INF/tools/languages/$languages/$languages.sh" ] && {
+    ! [[ -f "$TMPN/unzip/META-INF/tools/languages/$languages/$languages.sh" ]] && {
         abort_neo -e "23.27" -m "English language file not found, WHAT THE FUCK????"
     }
 fi
@@ -190,7 +188,7 @@ abort_neo() {
             ;;
         esac
     done
-    [ -n "$message" ] && {
+    [[ -n "$message" ]] && {
         my_print " "
         my_print " "
         my_print "- $message"
@@ -206,7 +204,7 @@ abort_neo() {
         error_code=$rounded_num
     fi
 
-    [ -n "$error_message" ] && {
+    [[ -n "$error_message" ]] && {
         [ -n "$word42" ] && {
             my_print "  !!!$word42: $error_message!!!"
         } || {
@@ -400,12 +398,17 @@ make_img() {
     done
     }
     wait
-  
     $TOOLS/make_ext4fs -J -T 1230764400 \
             -S "${FILE_CONTEXTS_FILE}" \
-            -l "$($TOOLS/busybox du -sb "${TARGET_DIR}" | $TOOLS/busybox awk '{print int($1*2)}')" \
+            -l "$($TOOLS/busybox du -sb "${TARGET_DIR}" | $TOOLS/busybox awk '{print int($1*50)}')" \
             -C "${FS_CONFIG_FILE}" -a "${LABLE}" -L "${LABLE}" \
             "$TMPN/${LABLE}.img" "${TARGET_DIR}"
+
+    resize2fs -M $TMPN/${LABLE}.img
+    resize2fs -M $TMPN/${LABLE}.img
+    resize2fs -M $TMPN/${LABLE}.img
+    resize2fs -f $TMPN/${LABLE}.img "$(($(stat -c%s $TMPN/${LABLE}.img)*2/512))"s
+        
 
 }
 
@@ -556,7 +559,6 @@ volume_selector() {
             abort_neo -e 2.1 -m "$word1"
         else
             error=true
-            ui_print " "
         fi
     done
 }
@@ -572,10 +574,10 @@ update_partitions(){
     if $bootctl_state ; then
         SLOTCURRENT=$($TOOLS/bootctl get-current-slot)
     else
-        SLOTCURRENT="$CSLOT"
+        SLOTCURRENT=""
     fi
 
-    if [ -z "$SLOTCURRENT" ] ; then 
+    if [[ -z "$SLOTCURRENT" ]] ; then
         case $CSLOT in 
             _a)
                 SLOTCURRENT=0
@@ -586,7 +588,7 @@ update_partitions(){
         esac 
     fi
 
-    if [ -n "$SLOTCURRENT" ] ; then 
+    if [[ -n "$SLOTCURRENT" ]] ; then 
         case "$SLOTCURRENT" in
             0) 
             SUFFIXCURRENT="_a"
@@ -847,7 +849,13 @@ remove_dfe_neo(){
                     # word26="Распаковка"
                     my_print "- $word26 $basename_block" && {
                         # word27="Не удалось распаковать"
-                        $TOOLS/magiskboot unpack -h $block &>$work_folder/log.unpack.boot || abort_neo -e "28.1" -m "$word27 boot($basename_block)" 
+                        $TOOLS/magiskboot unpack -h $block &>$work_folder/log.unpack.boot || {
+                        if [[ -n "$CSLOT" ]] ; then
+                            abort_neo -e "28.1" -m "$word27 boot($basename_block)" 
+                        else
+                            continue
+                        fi
+                        }    
 
                         if $TOOLS/toybox grep "RAMDISK_FMT" $work_folder/log.unpack.boot | $TOOLS/toybox grep "raw" &>$LOGNEO; then
                             # word28="Ramdisk сжат, декомпрессия..."
@@ -950,6 +958,15 @@ my_print "- $word52" && {
     fi
 }
 
+find_super_partition(){
+for blocksuper in /dev/block/by-name/* /dev/block/bootdevice/by-name/* /dev/block/bootdevice/* /dev/block/* ; do
+    if $TOOLS/lptools_new --super $blocksuper --get-info &>/dev/null; then
+        echo "$blocksuper"
+        break
+    fi    
+done 
+
+}
 $TOOLS/bootclt $>$LOGNEO
 if [[ "$?" == "64" ]] ; then
     bootctl_state=true
@@ -978,10 +995,11 @@ my_print "- $word2" && {
         "_b") CSLOT="_b" ;;
         *) CSLOT="" ;;
     esac
-    if ! find_block_neo -c -b super ; then
+    
+    export super_block=$(find_super_partition)
+    if [[ -z "$super_block" ]] ; then
         abort_neo -e "24.1" -m "$word3"
     fi
-    export super_block=$(find_block_neo -b super)
 
     install_after_ota=false
     if find_block_neo -c -b boot_a && find_block_neo -c -b boot_b && $SYS_STATUS ; then
@@ -1007,12 +1025,12 @@ my_print "- $word2" && {
 
 
     SWITCH_SLOT_AFTER_OTA=false
-    if ! $SYS_STATUS && [ -n $CSLOT ] ; then
+    if ! $SYS_STATUS && [[ -n "$CSLOT" ]] ; then
         update_partitions
         CSLOT=$(getprop ro.boot.slot_suffix)
-        if [ -z $CSLOT ]; then
+        if [[ -z "$CSLOT" ]]; then
             CSLOT=$(grep_cmdline androidboot.slot_suffix)
-            if [ -z $CSLOT ]; then
+            if [[ -z "$CSLOT" ]]; then
                 CSLOT=$(grep_cmdline androidboot.slot)
             fi
         fi
@@ -1107,7 +1125,13 @@ my_print "- $word2" && {
             $TOOLS/toybox mkdir -pv $work_folder
             cd "$work_folder"
             # word27="Не удалось распаковать"
-            $TOOLS/magiskboot unpack -h $block &>$work_folder/log.unpack.boot || abort_neo -e "28.1" -m "$word27 boot($basename_block)" 
+            $TOOLS/magiskboot unpack -h $block &>$work_folder/log.unpack.boot || {
+                        if [[ -n "$CSLOT" ]] ; then
+                            abort_neo -e "28.1" -m "$word27 boot($basename_block)" 
+                        else
+                            continue
+                        fi
+               }
 
             if $TOOLS/toybox grep "RAMDISK_FMT" $work_folder/log.unpack.boot | $TOOLS/toybox grep "raw" &>$LOGNEO; then
                     if $TOOLS/magiskboot decompress $work_folder/ramdisk.cpio $work_folder/ramdisk.decompress.cpio &>$work_folder/log.decompress.ramdisk ; then
@@ -1380,10 +1404,11 @@ my_print "- $word2" && {
     auto*)
         where_to_inject_auto="auto:"
         FLASH_IN_AUTO=true
+        where_to_inject=""
         if [[ -z "$CSLOT" ]]; then
             FLASH_IN_BOOT=false
         else 
-            where_to_inject=""
+            
             if ! find_block_neo -c -b "vendor_boot${RCSLOT}"; then
                 # word5="Не удалось найти, запись будет принудительно сделана в super"
                 my_print "- ${where_to_inject}${RCSLOT} $word70"
@@ -1393,7 +1418,7 @@ my_print "- $word2" && {
                 FLASH_IN_SUPER=false
                 FLASH_IN_BOOT=true
             fi
-            if [ -z "$where_to_inject" ] ; then 
+            if [[ -z "$where_to_inject" ]] ; then 
                 if ! find_block_neo -c -b "boot${RCSLOT}"; then
                     # word5="Не удалось найти, запись будет принудительно сделана в super"
                     my_print "- boot${RCSLOT} and vendor_boot${RCSLOT} $word5"
@@ -1405,6 +1430,10 @@ my_print "- $word2" && {
                     FLASH_IN_BOOT=true
                 fi
             fi
+        
+        fi 
+        if [[ -z "$where_to_inject" ]] ; then
+            where_to_inject=super
         fi
         ;;
     super*)
@@ -1502,7 +1531,13 @@ my_print "- $word71" && {
 
 # word12="Монтирование раздела Vendor"
 my_print "- $word12" && {
-    VENDOR_BLOCK=$(find_block_neo -b "vendor${CSLOT}")
+
+    if ! [ -h "$(readlink /dev/block/mapper/vendor${CSLOT})" ] && [ -b "$(readlink /dev/block/mapper/vendor${CSLOT})" ]; then
+        SYSTEM_BLOCK=("$(readlink /dev/block/mapper/vendor${CSLOT})")
+    fi
+    if [[ -z "$SYSTEM_BLOCK" ]] ; then
+        VENDOR_BLOCK=$(find_block_neo -b "vendor${CSLOT}")
+    fi
     # word13="Не удалось обноружить Vendor раздел"
     [[ -z "${VENDOR_BLOCK}" ]] && abort_neo -e 25.1 -m "$word13" 
     if ! $SYS_STATUS ; then
@@ -1537,11 +1572,11 @@ my_print "- $word14" && {
     else
         hardware_boot="$ro_hardware"
     fi
-    if [ -z "$hardware_boot" ]; then
+    if [[ -z "$hardware_boot" ]]; then
         hardware_boot=$(getprop ro.boot.hardware)
     fi
     
-    if [ -z "$hardware_boot" ]; then
+    if [[ -z "$hardware_boot" ]]; then
         hardware_boot=$(getprop ro.boot.hardware.platform)
     fi
     VENDOR_FOLDER="$full_path_to_vendor_folder"
@@ -1588,7 +1623,7 @@ my_print "- $word14" && {
         echo "End while2 $path_original_fstab:$basename_fstab" &>$LOGNEO
     done
     echo "$path_original_fstab:$basename_fstab" &>$LOGNEO
-    if [ -z "$path_original_fstab" ] || [ -z "$basename_fstab" ]; then
+    if [[ -z "$path_original_fstab" ]] || [[ -z "$basename_fstab" ]]; then
         # word15="Устройство не поддерживается"
 
         abort_neo -e 36.2 -m "$word15: $path_original_fstab:$basename_fstab"
@@ -1615,7 +1650,7 @@ my_print "- $word14" && {
 
     
 
-    if [ -f "${$full_path_to_vendor_folder}$(dirname ${path_original_fstab})/$basename_fstab" ]; then
+    if [[ -f "${$full_path_to_vendor_folder}$(dirname ${path_original_fstab})/$basename_fstab" ]]; then
         $TOOLS/busybox cp -afc "${$full_path_to_vendor_folder}$(dirname ${path_original_fstab})/$basename_fstab" "$TMPN/neo_inject${CSLOT}/$basename_fstab"
     else
         # word16="Не удалось определить расположение fstab:"
@@ -1628,7 +1663,7 @@ my_print "- $word14" && {
         # word17="Не найдено /userdata в fstab"
         abort_neo -e 36.4 -m "$word17"
     fi
-    if ! [ -f "$TMPN/neo_inject${CSLOT}/$basename_fstab" ] ; then
+    if ! [[ -f "$TMPN/neo_inject${CSLOT}/$basename_fstab" ]] ; then
         # word18="В процессе монтирования что то пошло не так"
         abort_neo -e 36.6 -m "$word18"
     fi
@@ -1656,13 +1691,13 @@ if $FLASH_IN_SUPER; then
             $TOOLS/lptools_new --super $super_block --slot 0 --suffix _b --remove "neo_inject_b" &>$LOGNEO
             $TOOLS/lptools_new --super $super_block --slot 1 --suffix _a --remove "neo_inject_a" &>$LOGNEO
             $TOOLS/lptools_new --super $super_block --slot 1 --suffix _b --remove "neo_inject_b" &>$LOGNEO
-            parametrs_lptools_final="--slot $CSLOTSLOT --suffix $CSLOT --create"
+            parametrs_lptools_final="--slot $CSLOTSLOT --suffix $CSLOT"
         else
             $TOOLS/lptools_new --super $super_block --remove "neo_inject${CSLOT}" &>$LOGNEO
             parametrs_lptools_final=""
         fi
         
-        if $TOOLS/lptools_new --super $super_block $parametrs_lptools_final "neo_inject${CSLOT}" "$SIZE_NEO_IMG" &>$LOGNEO; then
+        if $TOOLS/lptools_new --super $super_block $parametrs_lptools_final --create "neo_inject${CSLOT}" "$SIZE_NEO_IMG" &>$LOGNEO; then
             # my_print "- Разметка neo_inject${CSLOT} с размером $(awk 'BEGIN{printf "%.1f\n", '$SIZE_NEO_IMG'/1024/1024}')MB"
             if find_block_neo -b "neo_inject${CSLOT}"; then
                 cat "$NEO_IMG" >"/dev/block/mapper/neo_inject${CSLOT}"
@@ -1740,7 +1775,13 @@ for boot_sda in vendor_boot boot; do
             # word26="Распаковка"
             my_print "- $word26 $basename_block" && {
                 # word27="Не удалось распаковать"
-                $TOOLS/magiskboot unpack -h $block &>$work_folder/log.unpack.boot || abort_neo -e "28.1" -m "$word27 boot($basename_block)" 
+                $TOOLS/magiskboot unpack -h $block &>$work_folder/log.unpack.boot || {
+                        if [[ -n "$CSLOT" ]] ; then
+                            abort_neo -e "28.1" -m "$word27 boot($basename_block)" 
+                        else
+                            continue
+                        fi
+                   }
 
                 if $TOOLS/toybox grep "RAMDISK_FMT" $work_folder/log.unpack.boot | $TOOLS/toybox grep "raw" &>$LOGNEO; then
                     # word28="Ramdisk сжат, декомпрессия..."
