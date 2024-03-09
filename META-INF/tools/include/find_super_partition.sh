679d83dea64c145675fcfa1f32c03a3763c13904
Alexey Merzlyakov - LeeGarChat <leegarchat@gmail.com>
2024-03-09 18:02:06 +0300

    Фиксация от 09.03.24 18:02

--- META-INF/tools/include/find_super_partition.sh
+++
@@ -1,9 +0,0 @@
-
-
-
-for blocksuper in /dev/block/by-name/* /dev/block/bootdevice/by-name/* /dev/block/bootdevice/* /dev/block/* ; do
-    if lptools_new --super $blocksuper --get-info &>/dev/null; then
-        echo "$blocksuper"
-        break
-    fi    
-done 
