9224ff383d0aaee1d8b2317a81b88911e088e2ee
Alexey Merzlyakov - LeeGarChat <leegarchat@gmail.com>
2024-03-09 12:12:47 +0300

    Фиксация от 09.03.24 12:12

--- META-INF/tools/include/second_select_config_ask.sh
+++ META-INF/tools/include/second_select_config_ask.sh
@@ -73,34 +73,41 @@ if [[ $hide_not_encrypted == "ask" ]] ; then
         my_print " "
         my_print "- Удалить проверку целостности системы?"
         my_print "- ** Эта опция патчит vbmeta и system_vbmeta тем самым отключает проверку целостности системы, включите эту опцию если получили bootloop или если знаете зачем она нужна, в ином случае просто не трогайте"
-        my_print "    Да 'Отключить' - Громкость вверх (+)" -s
-        my_print "    Нет 'Не трогать' - Громкость вниз (-)" -s
+        my_print "    Да 'отключить' - Громкость вверх (+)" -s
+        my_print "    Нет 'не трогать' - Громкость вниз (-)" -s
         if volume_selector ; then
-            my_print "**> Да 'Отключить' - Громкость вверх (+)"
+            my_print "**> Да 'отключить' - Громкость вверх (+)"
             disable_verity_and_verification=true
         else
-            my_print "**> Нет 'Не трогать' - Громкость вниз (-)"
+            my_print "**> Нет 'не трогать' - Громкость вниз (-)"
             disable_verity_and_verification=false
         fi
     fi
 
     if [[ $zygisk_turn_on == "ask" ]] ; then
         my_print " "
-        my_print "- $word84"
-        my_print "    $word65" -s
-        my_print "    $word66" -s
+        my_print "- Принудительно включить zygisk во время включения?"
+        my_print "- ** Опция будет работать только если включен maghisk"
+        my_print "    Да 'включить' - Громкость вверх (+)" -s
+        my_print "    Нет 'не надо' - Громкость вниз (-)" -s
         if volume_selector ; then
+            my_print "**> Да 'включить' - Громкость вверх (+)"
             zygisk_turn_on=true
             my_print " "
-            my_print "- $word85"
-            my_print "    $word86" -s 
-            my_print "    $word87" -s
+            my_print "- Какой режим принудительного запуска использовать?"
+            my_print "- ** Постоянный, это значит что будет включаться каждый раз при запуске системы"
+            my_print "- ** Одноразовый, это значит что запуститься будет включен только при первом запуске системы, в дальнейшом будет игнорироваться принудительный запуск"
+            my_print "    'Постоянно' - Громкость вверх (+)" -s
+            my_print "    'Одноразово' - Громкость вниз (-)" -s
             if volume_selector ; then
-                zygisk_turn_on_parm=first_time_boot
-            else
+                my_print "**> 'Постоянно' - Громкость вверх (+)"
                 zygisk_turn_on_parm=always_on_boot
+            else    
+                my_print "**> 'Одноразово' - Громкость вниз (-)"
+                zygisk_turn_on_parm=first_time_boot
             fi
         else
+            my_print "**> Нет 'не надо' - Громкость вниз (-)"
             zygisk_turn_on=false
         fi
     elif [[ "$zygisk_turn_on" == "first_time_boot" ]] || [[ "$zygisk_turn_on" == "always_on_boot" ]] ; then
@@ -109,21 +116,28 @@ if [[ $hide_not_encrypted == "ask" ]] ; then
     fi
     if [[ $add_custom_deny_list == "ask" ]] ; then
         my_print " "
-        my_print "- $word88"
-        my_print "    $word65" -s
-        my_print "    $word66" -s
+        my_print "- Принудительно делать запись в denylist во время включения?"
+        my_print "- ** Опция будет работать только если включен zygisk"
+        my_print "    Да 'включить' - Громкость вверх (+)" -s
+        my_print "    Нет 'не надо' - Громкость вниз (-)" -s
         if volume_selector ; then
+            my_print "**> Да 'включить' - Громкость вверх (+)"
             add_custom_deny_list=true
             my_print " "
-            my_print "- $word89"
-            my_print "    $word90" -s
-            my_print "    $word91" -s
+            my_print "- Какой режим принудительного запуска использовать?"
+            my_print "- ** Постоянный, это значит что будет включаться каждый раз при запуске системы"
+            my_print "- ** Одноразовый, это значит что запуститься будет включен только при первом запуске системы, в дальнейшом будет игнорироваться принудительный запуск"
+            my_print "    'Постоянно' - Громкость вверх (+)" -s
+            my_print "    'Одноразово' - Громкость вниз (-)" -s
             if volume_selector ; then
+                my_print "**> 'Постоянно' - Громкость вверх (+)"
                 add_custom_deny_list_parm=first_time_boot
             else
+                my_print "**> 'Одноразово' - Громкость вниз (-)"
                 add_custom_deny_list_parm=always_on_boot
             fi
         else
+            my_print "**> Нет 'не надо' - Громкость вниз (-)"
             add_custom_deny_list=false
         fi
     elif [[ "$add_custom_deny_list" == "first_time_boot" ]] || [[ "$add_custom_deny_list" == "always_on_boot" ]] ; then
